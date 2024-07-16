b658a5f190207caa24bf856fdaf3a08bc514a800
Ho <dt.gael.ho@gmail.com>
2024-07-16 11:08:15 +0200

    coursSymfony : employeController, utilisation des repository

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,20 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    //public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        $name = 'Elan Formation';
-        $tableau = ["valeur 1", "valeur 2", "valeur 3", "valeur 4"];
+        
+        //$entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        //$entreprises = $entrepriseRepository->findAll();
+        //SELECT * FROM entreprise >HERE ville ='Strasbourg' ORDER BY raisonSociales
+        $entreprises = $entrepriseRepository->findBy(["ville" => "Strasbourg"],["raisonSociale" => "ASC"]);
         return $this->render('entreprise/index.html.twig', [
-            'name' => $name,
-            'tableau' => $tableau
+            'entreprises' => $entreprises
         ]);
     }
 }
